5fc0d22833238570fa707dbf2017fb421c1ecfa8
Oleh <kravetsoleh4991@gmail.com>
2023-05-11 22:55:42 -0700

    git@github.com:olehkravets4991/3rdhomework.git

---
+++ answers.js
@@ -0,0 +1,78 @@
+/////////////////////
+//4 - I feel pretty good about the material, but some extra review would help
+//////////////////////
+
+//////////////////////////
+for(let i=1; i<=20; i++){
+    console.log(i)
+}
+///////////////////////////
+
+//////////////////////////
+for(i=0; i<=200; i++){
+    console.log(i,i % 2 === 0 )
+    if(i % 2 === 0){
+        console.log(i)
+    }
+}
+///////////////////////////////
+
+////////////////////////////////////
+for(i=1; i<100; i++){
+    if(i % 3 === 0);
+    console.log("Fizz");
+    if(i % 5 === 0);
+    console.log("Buzz");
+    if(i % 3 === 0 && i % 5  === 0)
+    console.log("FizzBuzz")
+}
+//////////////////////////////////
+
+/////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down", "Hawkings"]
+plantee.splice(2,5001)
+console.log(plantee)
+wolfy.splice(3, "Gotham City")
+console.log(wolfy)
+wolfy.splice(0,"Gameboy")
+console.log(wolfy)
+////////////////////////////////
+
+//////////////////////////
+let turtles = ["Donatello","Leonardo","Raphael","Michelangelo"]
+for(let turtle of turtles){
+    let capilizedTurtle = turtle.toUpperCase()
+    console.log(capitalizedTurtle)
+}
+/////////////////////////////////
+
+
+///////////////////////////////////////
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+console.log(8)
+///////////////////////////////////
+
+////////////////////////////////////////
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+                    whereIsWaldo.splice("Eggbert")
+                    whereIsWaldo.splice(1,2, "No One")
+                    console.log(3,1,1)
+//////////////////////////////
+
+////////////////////////////////
+for(let i=1; i<=20; i++){
+    i = 'Love me,pet me! HSSSSS!'
+    console.log(i)
+}
+/////////////////////////////////////
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+console.log(nums[Math.floor(nums.length / 2)])
+/////////////////////////////////////////
+
+
